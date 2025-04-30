9a6711fd17e7ce1acef1f9b82242cbf64c61da62
IsaacLusca <isaaclucas.bezerra@gmail.com>
2025-04-30 20:12:35 -0300

    instalacao sql, implementação das tabelas por meio de classe, versionando com migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate   
 
+# O módulo os é usado para interagir com o sistema operacional, como acessar variáveis de ambiente.
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# models serve para definir as classes que representam as tabelas do banco de dados
+from app import routes, models
