8e13944f390cd87c7dd00e761cd2f6e248df159e
IsaacLusca <isaaclucas.bezerra@gmail.com>
2026-07-17 12:21:51 -0300

    fix: startup com db.create_all() em vez de flask db upgrade (sqlite + render)

--- .flaskenv
+++
@@ -1,2 +0,0 @@
-FLASK_APP=scripts.py
-FLASK_ENV=development
--- render_start.sh
+++ render_start.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-export FLASK_APP=scripts.py
-
-flask db upgrade
-flask seed
+python startup.py
 
 exec gunicorn app:app --bind 0.0.0.0:${PORT:-8000}
